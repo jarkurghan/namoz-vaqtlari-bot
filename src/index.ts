ad2958b13bf3124ad5b147a3cb2454e8769371d5
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-01-12 20:51:27 +0500

    fixed issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,4 @@
 import { Hono } from "hono";
-import { handle } from "hono/vercel";
 import { handleUpdate } from "./bot";
 
 const app = new Hono();
@@ -7,6 +6,4 @@ const app = new Hono();
 app.post("/bot", async (c) => await handleUpdate(c));
 app.get("/", (c) => c.text("Hello Hono!"));
 
-export const config = { runtime: "edge" };
-export const POST = handle(app);
-export const GET = handle(app);
+export default app;
