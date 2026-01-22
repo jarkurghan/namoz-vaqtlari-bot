899f7c7b6d7be6ee3226f5b63031ed894b627c21
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-01-22 18:47:20 +0500

    some fixes

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
-export const BOT_TOKEN = process.env.BOT_TOKEN;
+export const BOT_TOKEN = process.env.BOT_TOKEN || "";
 export const ADMIN_CHAT = process.env.ADMIN_CHAT_ID || "";
 export const LOG_CHAT = process.env.LOG_CHAT_ID || "";
 export const ADMIN_ID = process.env.ADMIN_PRIVATE_CHAT_ID || "";
