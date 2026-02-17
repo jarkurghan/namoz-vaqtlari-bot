b85f40e703c9f5b7c3e7b39be55692648a5af153
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-02-18 01:44:30 +0500

    revert: istisno

--- src/scheduler-2.ts
+++ src/scheduler-2.ts
@@ -2,7 +2,7 @@ import { getData } from "./get-data";
 
 const sendPostWithButtons = async () => {
     const minute = new Date().getUTCMinutes();
-    await getData(minute - 1-35);
+    await getData(minute - 1);
 };
 
 sendPostWithButtons();
