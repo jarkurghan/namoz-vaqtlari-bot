a2ab9292d3fdf9f3acb1517f8df6b8a002745551
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-03-01 15:10:52 +0500

    istisno #2

--- src/scheduler-istisno.ts
+++ src/scheduler-istisno.ts
@@ -1,12 +1,14 @@
 import { cronJob } from "./send";
 
 const sendPrayerTimes = async () => {
-    await cronJob(4);
-    console.log(4);
-    await cronJob(5);
-    console.log(5);
-    // await cronJob(3);
-    // console.log(3);
+    await cronJob(6);
+    console.log(6);
+    await cronJob(7);
+    console.log(7);
+    await cronJob(8);
+    console.log(8);
+    await cronJob(9);
+    console.log(9);
 };
 
 sendPrayerTimes();
