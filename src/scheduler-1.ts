5fbc6b056660e2d3193a98ddb90f38718beb35eb
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-03-09 09:37:38 +0000

    auto close db connections; convert to webhook; and other changes

--- src/scheduler-1.ts
+++
@@ -1,8 +0,0 @@
-import { cronJob } from "./send";
-
-const sendPrayerTimes = async () => {
-    const hour = parseInt(new Date().toLocaleString("en-US", { timeZone: "Asia/Tashkent", hour: "2-digit", hour12: false }));
-    await cronJob(hour);
-};
-
-sendPrayerTimes();
